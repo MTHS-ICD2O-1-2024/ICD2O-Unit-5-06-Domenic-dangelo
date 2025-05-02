7ca8db5b9f0af1b946cc8f900f10aeff80820de2
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-02 17:29:08 +0000

    old computer multiplication

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 domeic dangelo All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function checks if the user has selected the random number from above.
+ */
+// eslint-disable-next-line no-unused-vars
+function calculate() {
+  // input
+  const firstUserInput = parseFloat(document.getElementById("firstNumber").value)
+  const secondUserInput = parseFloat(document.getElementById("secondNumber").value)
+  let answer = 0
+
+  // process
+  while (firstUserInput >= 1) {
+    answer = answer + secondUserInput
+    firstUserInput --
+  }
+  // output
+  document.getElementById("result").innerHTML =
+  'Your number is:' + answer + '.'
+}
